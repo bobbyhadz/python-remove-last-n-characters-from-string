7ce850f0c0fdc272291c1e87d8c44d0fbc407a07
bobbyhadz <opmftw@gmail.com>
2024-04-10 17:27:39 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Remove the last N characters from a String in Python
+
+string = 'bobbyhadz.com'
+
+# ✅ Remove the last 2 characters from string
+new_string = string[:-2]
+print(new_string)  # 👉️ bobbyhadz.c
+
+# ✅ Remove the last 3 characters from a string
+new_string = string[:-3]
+print(new_string)  # 👉️ bobbyhadz.
+
+# ✅ Remove the last 4 characters from a string
+new_string = string[:-4]
+print(new_string)  # 👉️ bobbyhadz
